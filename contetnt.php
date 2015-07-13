9a73f644e2723c8a84aa3a588e801a4ac7ec432d
Orhan Ibrahimagic <oibrahimagic@capeannenterprises.com>
2015-07-13 17:53:29 +0200

    Changes made in lot of files

--- contetnt.php
+++ contetnt.php
@@ -1,9 +1,29 @@
-<html>
-	<head>
-		<title>Basic</title>
-	</head>
-	<body>
-
-	</body>
-</html>
-
+<?php require_once ("includes/connection.php"); ?>
+<?php require_once ("includes/functions.php"); ?>
+<?php include ("includes/header.php"); ?>
+			<table id="structure">
+				<tr>
+					<td id="navigation">
+						<?php
+						//3. Perform database querry
+						$result=mysqli_query($connection, "SELECT * FROM subjects");
+						if(!$result){
+							die("Database querry failed: " . mysql_error());
+						}
+						// 4. Use returned data
+						while($row=mysqli_fetch_array($result)){
+							echo $row["menu_name"]." ".$row["position"]. "<br/>";
+						}
+						?>
+					</td>
+					<td id="page">
+						<h2>Content Area</h2>
+					
+					</td>
+				</tr>
+			</table>
+<?php include("includes/footer.php"); ?>
+<?php
+	// 5. Close connection
+	mysqli_close($connection);
+?>
---
+++ includes/constants.php
@@ -0,0 +1,7 @@
+<?php
+//Database constants
+define("DB_SERVER", "localhost");
+define("DB_USER", "root");
+define("DB_PASS", "harun1");
+define("DB_NAME", "widget_corp");
+?>
